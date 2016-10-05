62aeed39de9a5b8819ac3b78a608c2419fcba493
romanyakovlev <yakovlevroman8@gmail.com>
2016-10-06 02:12:46 +0700

    коммит

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,15 @@
 import json
-
+from pprint import pprint
 
 def load_data(filepath):
-    pass
-
+    data = []
+    with open(filepath) as data_file:
+        data = json.load(data_file)
+    return data
 
 def pretty_print_json(data):
-    pass
-
+    pprint(data)
 
 if __name__ == '__main__':
-    pass
+    data = load_data('data.json')
+    pretty_print_json(data)
