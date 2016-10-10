b92ec884d66094c3f2ab515ac16848353e27a895
romanyakovlev <yakovlevroman8@gmail.com>
2016-10-10 18:34:48 +0700

    использование json pretty print в качестве вывода данных

--- pprint_json.py
+++ pprint_json.py
@@ -1,15 +1,20 @@
+# -*- coding: utf-8 -*-
+
 import json
-from pprint import pprint
+import argparse
 
 def load_data(filepath):
-    data = []
     with open(filepath) as data_file:
         data = json.load(data_file)
     return data
 
 def pretty_print_json(data):
-    pprint(data)
+    return json.dumps(data,indent=4, sort_keys=True,ensure_ascii=False)
 
 if __name__ == '__main__':
-    data = load_data('data.json')
-    pretty_print_json(data)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_name')
+    file_name = parser.parse_args().file_name
+    data = load_data(file_name)
+    print('Ваш файл c данными: {}'.format(file_name))
+    print(pretty_print_json(data))
